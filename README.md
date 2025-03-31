62805f3ede4522dbad59de0c93bfc2099395c3d8
jgose_macbook <jasongose@gmail.com>
2025-03-31 08:24:45 -0700

    add readme and cleaned out some experimenting for forceasted snow totals

---
+++ README.md
@@ -0,0 +1,5 @@
+## streamlit weather forcast app
+
+Testing streamlit and plotly for easy front-ends and visualization. Also used this to test out the open weather map API.
+Still looking for an appropriate API service to build separate app for the purpose of checking upcoming ski conditions
+at local resorts.
--- backend.py
+++ backend.py
@@ -9,8 +9,8 @@ def get_data(place, forecast_days=None):
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
+
     return filtered_data
 
 if __name__=="__main__":
-    print(get_data(place="Tokyo", forecast_days=2, kind="Temperature"))
-
+    print(get_data(place="flagstaff", forecast_days=2))
